eb331309d2a1b89e18ba1f7d142b507f326d5f90
samuelalejo18 <samuelalejomonsalve@gmail.com>
2024-08-05 10:09:46 -0500

    se calcula la multiplicación

--- MultiplicarNumeros/src/main/java/com/mycompany/multiplicarnumeros/MultiplicarNumeros.java
+++ MultiplicarNumeros/src/main/java/com/mycompany/multiplicarnumeros/MultiplicarNumeros.java
@@ -1,16 +1,18 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.multiplicarnumeros;
 
-/**
- *
- * @author Usuario
- */
+import java.util.Scanner;
+
 public class MultiplicarNumeros {
 
     public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n1,n2,M;
+        System.out.println("ingrese primer numero a multiplicar");
+        n1=sc.nextInt();
+        System.out.println("ingrese el segundo numero a multiplicar");
+        n2=sc.nextInt();
+        M=n1*n2;
+        System.out.println("su multiplicacion es "+M);
         System.out.println("Hello World!");
     }
 }
